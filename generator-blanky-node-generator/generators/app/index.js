8f9e306b83729fba88535ed5272b49bb40436df3
Joni Collinge <jonathan-collinge@hotmail.com>
2016-02-06 08:57:53 -0800

    Outputting valid SF service package

--- generator-blanky-node-generator/generators/app/index.js
+++ generator-blanky-node-generator/generators/app/index.js
@@ -2,6 +2,11 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var util = require('util');
+var ScriptBase = require('./script-base.js');
+var Glob = require("glob").Glob;
+var ncp = require('ncp').ncp;
+var mkdirp = require('mkdirp');
 
 module.exports = yeoman.generators.Base.extend({
     prompting: function () {
@@ -14,16 +19,28 @@ module.exports = yeoman.generators.Base.extend({
 
         var prompts = [
             {
-                type: 'input',
+                type: 'list',
                 name: 'platformType',
                 message: 'What type of application would you like to generator?',
-                default: "node"
+                default: "node",
+                choices: [{
+                    name: "node"
+                },
+                    {
+                        name: "c#"
+                    },
+                    {
+                        name: "java"
+                    },
+                    {
+                        name: "go"
+                    }]
             },
             {
                 type: 'input',
                 name: 'pkgName',
                 message: 'What is your app name?',
-                default: "blanky-service"
+                default: "BlankyService"
             },
             {
                 type: 'input',
@@ -42,11 +59,56 @@ module.exports = yeoman.generators.Base.extend({
         var pkgName = this.props.pkgName;
         var pkgVer = this.props.pkgVer;
 
-        console.log("application name: " + pkgName);
-        console.log("application version: " + pkgVer);
-    },
+        console.log("Creating application " + pkgName + ":v" + pkgVer + "...");
+        
+        var that = this;
+        
+        mkdirp(this.destinationPath(pkgName), function (err) {
 
+            mkdirp(that.destinationPath(pkgName + "/Code"), function (err) {
+
+                mkdirp(that.destinationPath(pkgName + "/Config"), function (err) {
+                    that.fs.copyTpl(
+                        that.templatePath('node/applicationmanifest.xml'),
+                        that.destinationPath('applicationmanifest.xml'),
+                        {
+                            Name: pkgName,
+                            Version: pkgVer
+                        }
+                        );
+
+                    that.fs.copyTpl(
+                        that.templatePath('node/servicemanifest.xml'),
+                        that.destinationPath(pkgName + '/servicemanifest.xml'),
+                        {
+                            Name: pkgName,
+                            Version: pkgVer
+                        }
+                        );
+
+                    ncp(that.templatePath("node/Code/"), that.destinationPath(pkgName + "/Code"), function (err) {
+                        if (err) {
+                            return console.error(err);
+                        }
+                        console.log('Copied code files');
+                    });
+
+                    ncp(that.templatePath("node/Config/"), that.destinationPath(pkgName + "/Config"), function (err) {
+                        if (err) {
+                            return console.error(err);
+                        }
+                        console.log('Copied config files');
+                    });
+
+                });
+            });
+        });
+
+    },
     install: function () {
-        this.installDependencies();
+        //this.installDependencies();
+    },
+    final: function () {
+        console.log("Created application");
     }
 });
---
+++ generator-blanky-node-generator/generators/app/script-base.js
@@ -0,0 +1,26 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+
+var NamedGenerator = module.exports = function NamedGenerator() {
+    yeoman.generators.NamedBase.apply(this, arguments);
+    this.sourceRoot(path.join(__dirname, './templates/'));
+
+    this.namespace = function () {
+        return require('./configuration').getNamespace(this.fs);
+    }.bind(this);
+};
+
+util.inherits(NamedGenerator, yeoman.generators.NamedBase);
+
+NamedGenerator.prototype.generateTemplateFile = function (templateFile, targetFile, templateData) {
+    this.log('You called the aspnet subgenerator with the arg ' + chalk.green(this.arguments[0] || targetFile));
+    if (templateData !== null) {
+        this.fs.copyTpl(this.templatePath(templateFile), this.destinationPath(targetFile), templateData);
+    } else {
+        this.fs.copyTpl(this.templatePath(templateFile), this.destinationPath(targetFile));
+    }
+    this.log(chalk.green(targetFile) + ' created.');
+};
